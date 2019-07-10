c3d20d177d832fa84acb8011b2205c8439a6d87d
andela-sjames <samuel.james@andela.com>
2019-07-10 15:41:34 +0100

    Proof of concept.

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/fraugster/observability-challenge
+
+require (
+	github.com/lib/pq v1.0.0
+	github.com/prometheus/client_golang v1.0.0
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const (
+	host   = "db"
+	port   = 5432
+	user   = "postgres"
+	dbname = "postgres"
+)
+
+// RED Pattern
+// Request Rate
+// Error rate
+// Duration
+
+var (
+	dbRequestsDuration = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "Db_request_duration_seconds",
+		Help: "The duration of the requests to the DB service.",
+	})
+
+	dbRequestsCurrent = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "Db_requests_current",
+		Help: "The current number of requests to the DB service.",
+	})
+
+	dbClientErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "Db_errors",
+		Help: "The total number of DB client errors",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(dbRequestsDuration, dbRequestsCurrent, dbClientErrors)
+}
+
+func metricsHandler(w http.ResponseWriter, r *http.Request, s string) {
+	// TODO expose metrics here
+	promhttp.Handler().ServeHTTP(w, r)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "" {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, "")
+	}
+}
+
+func main() {
+	password := os.Getenv("PGPASSWORD")
+
+	// setup DB connection
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Successfully connected to DB!")
+
+	// continously send db queries
+	go func(db *sql.DB) {
+		for {
+			now := time.Now()
+			dbRequestsCurrent.Inc()
+			log.Println("Sending sql query")
+			sqlStatement := "select pg_sleep(floor(random() * 10 + 1)::int);"
+			_, err = db.Exec(sqlStatement)
+			dbRequestsDuration.Observe(time.Since(now).Seconds())
+			if err != nil {
+				dbClientErrors.Inc()
+				panic(err)
+			}
+		}
+	}(db)
+
+	// setup handler
+	http.HandleFunc("/metrics/", makeHandler(metricsHandler))
+	log.Println("Port :8000 is ready for metrics collection")
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
